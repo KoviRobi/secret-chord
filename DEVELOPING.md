8a58165d0823233a2e4f9fa25f7625319b1dae23
Kovacsics Robert <kovirobi@gmail.com>
2024-09-08 01:52:08 +0100

    Add DEVELOPING.md

---
+++ DEVELOPING.md
@@ -0,0 +1,6 @@
+# Developing
+
+I have written this using my fork of git-appraise, see
+[github.com/KoviRobi/git-appraise](github.com/KoviRobi/git-appraise).
+
+To set up the worktrees, see [./setup-development.sh](./setup-development.sh).
---
+++ setup-development.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+set -eux
+
+if git worktree add reviews refs/notes/devtools/reviews; then
+  ( cd reviews; echo 'ref: refs/notes/devtools/reviews' > "`git rev-parse --git-dir`/HEAD" )
+fi
+if git worktree add discuss refs/notes/devtools/discuss; then
+  ( cd discuss; echo 'ref: refs/notes/devtools/discuss' > "`git rev-parse --git-dir`/HEAD" )
+fi
+if git worktree add archives refs/devtools/archives/reviews; then
+  ( cd archives; echo 'ref: refs/devtools/archives/reviews' > "`git rev-parse --git-dir`/HEAD" )
+fi
+
+git config --local author.email "🐶"
+git config --local committer.email "🐶"
+git config --local user.email "🐶"
+git config --local --add notes.rewriteRef refs/notes/devtools/reviews
+git config --local --add notes.rewriteRef refs/notes/devtools/discuss
+
+worktrees=(
+	1-introduction-base
+	1-introduction
+	2-going-forth-base
+	2-going-forth
+)
+
+for worktree in "${worktrees[@]}"; do
+	git worktree add "$worktree" "$worktree"
+done
