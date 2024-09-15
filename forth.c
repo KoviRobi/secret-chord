ec89e7c27b553f0915fa3069537366f15108e55e
Kovacsics Robert <>
2024-09-15 22:43:20 +0100

    Make it possible to define functions and call them
    By creating the return stack, we can call functions, and return to the
    point after we called them.
    
    TODO: We could have used C recursion, why didn't we?

--- forth.c
+++ forth.c
@@ -7,16 +7,28 @@ typedef void instruction(void **pc);
 
 int stack[100];
 unsigned stack_p;
+void **rstack[100];
+unsigned ret_p;
 
 void push(int value) { stack[stack_p++] = value; }
 int pop(void) { return stack[--stack_p]; }
+void rpush(void **value) { rstack[ret_p++] = value; }
+void **rpop(void) { return rstack[--ret_p]; }
 
 instruction lit;
+instruction call;
+instruction return_;
 instruction stop;
 
-instruction *prog[] = {
+instruction *method[] = {
 	&lit,
 	(instruction *)3,
+	&return_,
+};
+
+instruction *prog[] = {
+	&call,
+	(instruction *)method,
 	&stop,
 };
 
@@ -29,6 +41,13 @@ void lit(void **pc) {
 	push(*(int *)pc++);
 	next(pc);
 }
+
+void call(void **pc) {
+	rpush(pc + 1);
+	next(*(int *)pc);
+}
+
+void return_(void **pc) { next(rpop()); }
 
 void stop(void **pc) {}
 
